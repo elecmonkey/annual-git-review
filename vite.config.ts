4d805ad76e84ff20da7112ea851f63ae7b1b04c7
Elecmonkey <elecmonkey@hotmail.com>
2025-12-16 12:20:05 +0800

    chore: use port 2025

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  server: {
+    port: 2025
+  },
   ssr: {
     noExternal: ['@sveltejs/kit']
   }
